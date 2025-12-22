d58a1eb14107ce40dc413d3302a9dc3adc5b5f71
Mircea Ionescu <mihail.mircea12@gmail.com>
2025-12-22 16:53:30 +0100

    moved functions to file parisng file to free up main

---
+++ src/file_parsing.rs
@@ -0,0 +1,77 @@
+use rand::Rng;
+use std::path::PathBuf;
+
+use std::fs;
+use std::io;
+
+use crate::question_structs::{Question, QuestionsCount, QuestionType, Informative};
+
+const SMALL_QUESTION_CHANCE: f64 = 0.5;
+const BIG_QUESTION_CHANCE: f64 = 0.5;
+// const SMALL_QUESTION_CHANCE: f64 = 0.2;
+// const BIG_QUESTION_CHANCE: f64 = 0.1;
+
+// refactor to use an iterator that only reads until endl
+fn get_question_from_type(questions_file_path: &PathBuf, type_to_get: QuestionType, question_idx: i64) -> io::Result<Question> {
+    let all_questions = fs::read_to_string(questions_file_path)?;
+    let all_questions_it = all_questions.split("\n");
+
+    let mut q_idx = 0;
+    for question_str in all_questions_it {
+        let question: Question = question_str.to_string().into();
+        let question_type = question.get_type();
+
+        if type_to_get == question_type {
+            if q_idx == question_idx{
+                return Ok(question);
+            }
+            q_idx += 1;
+        }
+    }
+
+    Ok(Question::default())
+}
+
+fn get_questions_counts(questions_file_path: &PathBuf) -> io::Result<QuestionsCount> {
+    let mut q_count: QuestionsCount = QuestionsCount{short:0, long: 0, answer: 0};
+
+    let all_questions = fs::read_to_string(questions_file_path)?;
+    let all_questions_it = all_questions.split("\n");
+
+    for question_str in all_questions_it {
+        let question: Question = question_str.to_string().into();
+        let question_type = question.get_type();
+
+        match question_type{
+            QuestionType::Long => q_count.long += 1,
+            QuestionType::Short => q_count.short += 1,
+            QuestionType::Answer => q_count.answer += 1,
+            _ => continue,
+        }
+    }
+
+    Ok(q_count)
+}
+
+pub fn get_question(questions_file_path: PathBuf) -> io::Result<Question> {
+    // let small_questions: Vec<String> = get_question_type(questions_file_path.clone(), QuestionType::Short)?;
+    // let long_questions: Vec<String>= get_question_type(questions_file_path, QuestionType::Long)?;
+    let q_counts = get_questions_counts(& questions_file_path)?;
+
+    let mut rnd = rand::rng();
+
+    let question_length_sample: f64 = rnd.random();
+    let small_question_sample: i64 = (rnd.random::<f64>() * (q_counts.short as f64)) as i64;
+    let long_question_sample: i64 = (rnd.random::<f64>() * (q_counts.long as f64)) as i64;
+
+    match question_length_sample{
+        c if c < SMALL_QUESTION_CHANCE 
+            => return get_question_from_type(&questions_file_path, QuestionType::Short, small_question_sample),
+        c if c < (SMALL_QUESTION_CHANCE + BIG_QUESTION_CHANCE) 
+            => return get_question_from_type(&questions_file_path, QuestionType::Long, long_question_sample),
+        _ => (),
+    }
+
+    Ok(Question::default())
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,13 @@
 mod question_structs;
-use question_structs::{Question, QuestionsCount, QuestionType, Informative};
+use question_structs::{Question, Informative};
+
+mod file_parsing;
 
 use std::fs;
 use std::fs::OpenOptions;
 
 use std::io::{self, Write};
 use std::path::PathBuf;
-
-use rand::Rng;
-
-const SMALL_QUESTION_CHANCE: f64 = 0.5;
-const BIG_QUESTION_CHANCE: f64 = 0.5;
-// const SMALL_QUESTION_CHANCE: f64 = 0.2;
-// const BIG_QUESTION_CHANCE: f64 = 0.1;
 
 fn exists_today_file(jl_files_path: PathBuf, today_file: String) -> io::Result<bool> {
     let today_file_path = jl_files_path.join(&today_file);
@@ -30,70 +25,6 @@ fn exists_today_file(jl_files_path: PathBuf, today_file: String) -> io::Result<b
     Ok(false)
 }
  
-// refactor to use an iterator that only reads until endl
-fn get_question_from_type(questions_file_path: &PathBuf, type_to_get: QuestionType, question_idx: i64) -> io::Result<Question> {
-    let all_questions = fs::read_to_string(questions_file_path)?;
-    let all_questions_it = all_questions.split("\n");
-
-    let mut q_idx = 0;
-    for question_str in all_questions_it {
-        let question: Question = question_str.to_string().into();
-        let question_type = question.get_type();
-
-        if type_to_get == question_type {
-            if q_idx == question_idx{
-                return Ok(question);
-            }
-            q_idx += 1;
-        }
-    }
-
-    Ok(Question::default())
-}
-
-fn get_questions_counts(questions_file_path: &PathBuf) -> io::Result<QuestionsCount> {
-    let mut q_count: QuestionsCount = QuestionsCount{short:0, long: 0, answer: 0};
-
-    let all_questions = fs::read_to_string(questions_file_path)?;
-    let all_questions_it = all_questions.split("\n");
-
-    for question_str in all_questions_it {
-        let question: Question = question_str.to_string().into();
-        let question_type = question.get_type();
-
-        match question_type{
-            QuestionType::Long => q_count.long += 1,
-            QuestionType::Short => q_count.short += 1,
-            QuestionType::Answer => q_count.answer += 1,
-            _ => continue,
-        }
-    }
-
-    Ok(q_count)
-}
-
-fn get_question(questions_file_path: PathBuf) -> io::Result<Question> {
-    // let small_questions: Vec<String> = get_question_type(questions_file_path.clone(), QuestionType::Short)?;
-    // let long_questions: Vec<String>= get_question_type(questions_file_path, QuestionType::Long)?;
-    let q_counts = get_questions_counts(& questions_file_path)?;
-
-    let mut rnd = rand::rng();
-
-    let question_length_sample: f64 = rnd.random();
-    let small_question_sample: i64 = (rnd.random::<f64>() * (q_counts.short as f64)) as i64;
-    let long_question_sample: i64 = (rnd.random::<f64>() * (q_counts.long as f64)) as i64;
-
-    match question_length_sample{
-        c if c < SMALL_QUESTION_CHANCE 
-            => return get_question_from_type(&questions_file_path, QuestionType::Short, small_question_sample),
-        c if c < (SMALL_QUESTION_CHANCE + BIG_QUESTION_CHANCE) 
-            => return get_question_from_type(&questions_file_path, QuestionType::Long, long_question_sample),
-        _ => (),
-    }
-
-    Ok(Question::default())
-}
-
 fn main() -> io::Result<()> {
     let mut jl_files_path = PathBuf::new();
     jl_files_path.push("./.jl");
@@ -106,7 +37,7 @@ fn main() -> io::Result<()> {
         fs::write(&today_file_path, "")?;
     }
 
-    let question_to_ask = get_question(questions_file_path)?;
+    let question_to_ask = file_parsing::get_question(questions_file_path)?;
 
     if question_to_ask == Question::default(){
         return Ok(());
