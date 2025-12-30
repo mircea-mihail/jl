9267733bc14f80c8c584471b31d93857523eb990
Mircea Ionescu <mihail.mircea12@gmail.com>
2025-12-30 12:37:39 +0200

    Move jl dir to /home/user/.jl path Add default question file Start using arguments for voluntary user journaling

--- src/main.rs
+++ src/main.rs
@@ -20,11 +20,34 @@ use std::path::Path;
 use rustyline::error::ReadlineError;
 use rustyline::{DefaultEditor, Result, Config, EditMode};
 
-fn exists_today_file(jl_files_path: &Path, today_file: &String) -> io::Result<bool> {
-    let today_file_path = jl_files_path.join(&today_file);
+use clap::Parser;
 
-    if jl_files_path.is_dir() {
-        for entry in fs::read_dir(jl_files_path)? {
+use home;
+
+const JL_DIR_NAME: &str = ".jl";
+const QUESTION_FILE_NAME: &str = "questions.txt";
+
+#[derive(Parser)]
+struct Cli {
+    #[arg(short, long)]
+    description: Option<String>,
+
+    #[arg(short, long)]
+    rating: Option<f64>,
+}
+
+fn exists_today_file(jl_dir_path: &Path, today_file: &String) -> io::Result<bool> {
+    let today_file_path = jl_dir_path.join(&today_file);
+
+    if !jl_dir_path.is_dir() {
+        match fs::create_dir(jl_dir_path){
+            Ok(()) => (),
+            Err(e) => println!("error: {}", e),
+        }
+    }
+
+    if jl_dir_path.is_dir() {
+        for entry in fs::read_dir(jl_dir_path)? {
             let entry = entry?;
             let path = entry.path();
 
@@ -37,20 +60,44 @@ fn exists_today_file(jl_files_path: &Path, today_file: &String) -> io::Result<bo
 }
 
 fn main() -> Result<()> {
-    let jl_files_path = Path::new("./.jl");
+    let args = Cli::parse();
+
+    match args.description{
+        Some(a) => {
+            println!("description: {}", a);
+            return Ok(());
+        }
+        None => (),
+    }
+    match args.rating{
+        Some(a) => {
+            println!("rating: {}", a);
+            return Ok(());
+        }
+        None => (),
+    }
+
+    let mut jl_dir_path = home::home_dir().expect("Could not find home directory");
+    jl_dir_path.push(JL_DIR_NAME);
 
     let today_file= chrono::offset::Local::now().format("%Y-%m-%d.txt").to_string();
-    let today_file_path = jl_files_path.join(&today_file);
-    let questions_file_path = jl_files_path.join("questions.txt");
+    let today_file_path = jl_dir_path.join(&today_file);
+    let questions_file_path = jl_dir_path.join(QUESTION_FILE_NAME);
+    if !questions_file_path.exists() {
+        fs::write(&questions_file_path, "l: Long question\ns: Short question\n")
+            .expect("Failed to create question file\n"); 
+    }
 
-    if !exists_today_file(jl_files_path, &today_file)? {
+    if !exists_today_file(&jl_dir_path, &today_file)? {
         fs::write(&today_file_path, "")?;
     }
+
     let question_to_ask: Question = file_parsing::get_question(&questions_file_path, &today_file_path)?;
 
     if question_to_ask == Question::default(){
         return Ok(());
     }
+    // refactor, put into function
     println!("{}", question_to_ask.get_text());
 
     let config = Config::builder()
@@ -61,9 +108,10 @@ fn main() -> Result<()> {
     let mut file = OpenOptions::new()
         .write(true)    
         .append(true)   
-        .open(today_file_path)?;
+        .open(&today_file_path)?;
 
     let mut wrote_quesiton = false;
+
     loop {
         let readline = rl.readline(">> ");
         match readline {
@@ -99,5 +147,6 @@ fn main() -> Result<()> {
             }
         }
     }
+    println!("end of file");
     Ok(())
 }
