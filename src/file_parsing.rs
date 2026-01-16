0311c70dd49921718c3c4819f29b6a94ecd3b0ee
Mircea Ionescu <mihail.mircea12@gmail.com>
2026-01-16 11:53:16 +0100

    delete commented question generating code

--- src/file_parsing.rs
+++ src/file_parsing.rs
@@ -11,50 +11,6 @@ use crate::question_structs::{Informative, Question};
 
 use rand::seq::SliceRandom; 
 
-// fn get_question_vector(
-//     questions_path: &Path,
-//     get_type: &QuestionType,
-// ) -> io::Result<Vec<Question>> {
-//     let all_questions = fs::read_to_string(questions_path)?;
-//     let all_questions_it = all_questions.split("\n");
-
-//     let mut q_vec: Vec<Question> = Vec::new();
-
-//     for question_str in all_questions_it {
-//         let question: Question = question_str.to_string().into();
-//         let question_type = question.get_type();
-
-//         if get_type == &question_type {
-//             q_vec.push(question);
-//         }
-//     }
-
-//     Ok(q_vec)
-// }
-
-// // returns a vector of type that only has different questions than the ones in asked_questions
-// fn get_unasked_question_vector(
-//     questions_path: &Path,
-//     get_type: &QuestionType,
-//     asked_questions: Vec<Question>,
-// ) -> io::Result<Vec<Question>> {
-//     let all_questions = fs::read_to_string(questions_path)?;
-//     let all_questions_it = all_questions.split("\n");
-
-//     let mut q_vec: Vec<Question> = Vec::new();
-
-//     for question_str in all_questions_it {
-//         let question: Question = question_str.to_string().into();
-//         let question_type = question.get_type();
-
-//         if get_type == &question_type && !asked_questions.contains(&question) {
-//             q_vec.push(question);
-//         }
-//     }
-
-//     Ok(q_vec)
-// }
-
 fn generate_jumbled_questions_file(questions_path: &Path, jumbled_questions_path: &Path) -> io::Result<()>{
     let questions_text = fs::read_to_string(questions_path)?;
     let mut questions: Vec<Question> = Vec::new();
@@ -81,8 +37,6 @@ fn generate_jumbled_questions_file(questions_path: &Path, jumbled_questions_path
 
 pub fn get_question(
     questions_path: &Path,
-    // today_file_path: &Path,
-    // question_chances: QuestionChances,
 ) -> io::Result<Question> {
     let jumbled_questions_path = std::path::PathBuf::from("/tmp")
         .join(utility::get_day_file_name(0));
@@ -94,15 +48,6 @@ pub fn get_question(
         generate_jumbled_questions_file(&questions_path, &jumbled_questions_path)?;
     }
 
-    // let mut rnd = rand::rng();
-
-    // let question_length_sample: f32 = rnd.random();
-
-    // let question_type: QuestionType = match question_length_sample {
-    //     c if c < question_chances.short => QuestionType::Short,
-    //     c if c < (question_chances.short + question_chances.long) => QuestionType::Long,
-    //     _ => return Ok(Question::default()),
-    // };
     let all_questions = fs::read_to_string(&jumbled_questions_path)?;
 
     let mut file: fs::File = OpenOptions::new()
